a1d700d5227b5a377c31110f64a7ef0142fb5020
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2016-09-30 14:13:43 -0400

    adding libpostal to api views

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from postal.expand import expand_address
+from postal.parser import parse_address
 from rest_framework import status
 from rest_framework.views import APIView
 
@@ -13,9 +15,9 @@ class ExpandAPIView(APIView):
             }
             return JsonResponse(error_message, status=status.HTTP_400_BAD_REQUEST)
 
-        # expanded = expand_address()
+        expanded_list = expand_address(address)
         expanded = {
-            'results': ['ex', 'expa', 'expand', 'expanded'],
+            'results': expanded_list,
         }
         return JsonResponse(expanded, status=status.HTTP_200_OK)
 
@@ -29,7 +31,6 @@ class ParseAPIView(APIView):
                 'error': 'You must pass `address` as a query parameter.'
             }
             return JsonResponse(error_message, status=status.HTTP_400_BAD_REQUEST)
-        # parser_results = parse_address()
-        parser_results = [('381', 'house_number'), ('new york', 'city')]
+        parser_results = parse_address(address)
         parsed_dict = {item[1]: item[0] for item in parser_results}
         return JsonResponse(parsed_dict, status=status.HTTP_200_OK)
