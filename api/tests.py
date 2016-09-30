8d49bb8ffb0136a2378ea5f74b7cff44d1171c2a
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2016-09-30 13:47:38 -0400

    pre-libpostal tests written

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import SimpleTestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class PostalAPITestMixin():
+    view_name = None
+
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse(self.view_name)
+        self.address = '123 Postal St, Lib Postal, NY 10001 USA'
+
+    def test_get_with_no_params_returns_400(self):
+        response = self.client.get(self.url)
+        assert response.status_code == 400
+
+    def test_get_with_address_param_returns_200(self):
+        response = self.client.get(self.url, {'address': self.address})
+        assert response.status_code == 200
+
+    def test_get_with_address_and_other_params_returns_200(self):
+        query_params = {
+            'address': self.address,
+            'lib': 'postal',
+            'city': 'new york',
+        }
+        response = self.client.get(self.url, query_params)
+        assert response.status_code == 200
+
+    def test_get_with_other_params_returns_400(self):
+        response = self.client.get(self.url, {'target': self.address})
+        assert response.status_code == 400
+
+
+class ExpandAPIViewTestCase(PostalAPITestMixin, SimpleTestCase):
+    view_name = 'expand'
+
+
+class ParseAPIViewTestCase(PostalAPITestMixin, SimpleTestCase):
+    view_name = 'parse'
--- api/views.py
+++ api/views.py
@@ -5,11 +5,31 @@ from rest_framework.views import APIView
 
 class ExpandAPIView(APIView):
     def get(self, request, format=None):
-        expanded = {'hey': 'expanded'}
+        try:
+            address = request.query_params['address']
+        except KeyError:
+            error_message = {
+                'error': 'You must pass `address` as a query parameter.'
+            }
+            return JsonResponse(error_message, status=status.HTTP_400_BAD_REQUEST)
+
+        # expanded = expand_address()
+        expanded = {
+            'results': ['ex', 'expa', 'expand', 'expanded'],
+        }
         return JsonResponse(expanded, status=status.HTTP_200_OK)
 
 
 class ParseAPIView(APIView):
     def get(self, request, format=None):
-        expanded = {'hey': 'parsed'}
-        return JsonResponse(expanded, status=status.HTTP_200_OK)
+        try:
+            address = request.query_params['address']
+        except KeyError:
+            error_message = {
+                'error': 'You must pass `address` as a query parameter.'
+            }
+            return JsonResponse(error_message, status=status.HTTP_400_BAD_REQUEST)
+        # parser_results = parse_address()
+        parser_results = [('381', 'house_number'), ('new york', 'city')]
+        parsed_dict = {item[1]: item[0] for item in parser_results}
+        return JsonResponse(parsed_dict, status=status.HTTP_200_OK)
