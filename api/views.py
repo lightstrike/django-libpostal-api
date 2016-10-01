9dbee7f50eb45887f78ade4e8216391b6958b412
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2016-09-30 22:04:27 -0400

    raw or ordered dict options for parser endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from django.http import JsonResponse
 from postal.expand import expand_address
 from postal.parser import parse_address
@@ -32,5 +34,10 @@ class ParseAPIView(APIView):
             }
             return JsonResponse(error_message, status=status.HTTP_400_BAD_REQUEST)
         parser_results = parse_address(address)
-        parsed_dict = {item[1]: item[0] for item in parser_results}
+        if 'raw' in request.query_params:
+            parsed_dict = OrderedDict(parser_results)
+        else:
+            # Reverse items to get one unique classification key per query
+            reversed_items = [(item[1], item[0]) for item in parser_results]
+            parsed_dict = OrderedDict(reversed_items)
         return JsonResponse(parsed_dict, status=status.HTTP_200_OK)
